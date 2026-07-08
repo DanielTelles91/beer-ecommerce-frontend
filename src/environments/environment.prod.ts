a0e697660b72216ff8c93b4a2c931c4171866bd0
Daniel A. Telles <daniel.telles91@yahoo.com>
2026-07-07 22:27:13 -0300

    fixed apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://beer-ecommerce-platform.onrender.com/'
+    apiUrl: 'https://beer-ecommerce-platform.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,6 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:8080'
     apiUrl: 'http://192.168.1.131:8080'
+
+   
 };
